aafc35855afda745ee16d14243d32e63720a59b7
Fleur41 <Muriithikings456@gmail.com>
2024-01-04 11:54:02 +0300

    Final reddit

--- Red.py
+++ Red.py
@@ -9,11 +9,11 @@ import enchant
 
 def reddit_bot(sub, trigger_phrase):
     reddit = praw.Reddit(
-        client_id = 'your_client_id',
-        client_secret = 'your_client_secret',
-        username = 'your_username',
-        password = 'your_pw',
-        user_agent = 'your_user_agent'
+        client_id='your_client_id',
+        client_secret='your_client_secret',
+        username='your_username',
+        password='your_password',
+        user_agent='your_user_agent'
     )
 
     subreddit = reddit.subreddit(sub)
@@ -25,4 +25,8 @@ def reddit_bot(sub, trigger_phrase):
             reply_text = ''
             similar_words = dict_suggest.suggest(word)
             for similar in similar_words:
-                
+                reply_text += (similar + '')
+            comment.reply(reply_text)
+
+if __name__ == '__main__':
+    reddit_bot(sub='Python', trigger_phrase='useful bot')
