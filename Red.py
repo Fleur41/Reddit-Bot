f2c6b2ddc51209ef3b37accf5e61fd94befce16c
Fleur41 <Muriithikings456@gmail.com>
2024-01-04 11:40:02 +0300

    reddit_bot

--- Red.py
+++ Red.py
@@ -7,3 +7,11 @@ pip install praw pyenchant
 import praw
 import enchant
 
+def reddit_bot(sub, trigger_phrase):
+    reddit = praw.Reddit(
+        client_id = 'your_client_id',
+        client_secret = 'your_client_secret',
+        username = 'your_username',
+        password = 'your_pw',
+        user_agent = 'your_user_agent'
+    )
